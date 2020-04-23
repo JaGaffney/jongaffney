02592e24a053eff1326e2d1202b389b29f287f1a
JaGaffney <jagaffney661@msn.com>
2020-04-23 16:47:42 +1000

    added basic hero image and precursor for styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import "./src/styles/index.scss"
+import "./src/styles/layout.scss"
+import "./src/styles/generics.scss"
+import "./src/styles/header.scss"
+import "./src/styles/footer.scss"
+import "./src/styles/hero.scss"
+import "./src/styles/responsive.scss"
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import LogoImage from "../../images/logo.svg"
+import HeroImage from "../../images/hero.png"
+
+const Hero = () => {
+  return (
+    <header className="hero__container">
+      <img src={HeroImage} alt="" className="hero__image" />
+
+      <div className="hero__content-container">
+        <div className="hero__content">
+          <img src={LogoImage} alt="logo" />
+          <div className="hero__content-text">
+            <h1>
+              Fullstack <span>web Developer</span>
+            </h1>
+            <h3>
+              From static sites to full web applications and everything in
+              between
+            </h3>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Hero
