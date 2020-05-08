6b29a6843af70f034b5536579c2025f9b1aa1f9e
JaGaffney <jagaffney661@msn.com>
2020-05-08 17:48:26 +1000

    changed button to link

--- src/components/Landing/ContactBanner.js
+++ src/components/Landing/ContactBanner.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
 
 const ContactBanner = () => {
   const [contactEmail, setContactEmail] = useState("")
@@ -14,13 +15,13 @@ const ContactBanner = () => {
   return (
     <div className="contact-banner-container" id="landing-contact">
       <span>If your in need of my services</span>
-      <form action={`/contact`} method="POST">
+      <form>
         <input
           placeholder="john-smith@email.com.au"
           onChange={e => setContactEmail(e.target.value)}
           value={contactEmail}
         ></input>
-        <button>Get in touch</button>
+        <Link to="/contact">Get in touch</Link>
       </form>
     </div>
   )
