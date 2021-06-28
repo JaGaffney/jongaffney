6e3569e70700a1ebfd409b207cbe0b4e93c97bb9
JaGaffney <jagaffney661@msn.com>
2021-06-28 12:55:59 +1000

    refactor: made tags more diverse

--- src/components/Landing/Apps.js
+++ src/components/Landing/Apps.js
@@ -19,14 +19,14 @@ const Apps = () => {
     description:
       "Websites created with the purpose of being hooked up to a database. Ideal for e-commerce, stock-management or data analysis websites.",
     image: AfnafImage,
-    tags: ["React", "Django", "Laravel", "MongoDB", "MySQL"],
+    tags: ["React", "Django", "Flask", "NoSQL", "SQL"],
   }
   const staticSites = {
     name: "Statically generated",
     description:
       "Static websites that are easy to host and display your message, to your customers. Created to allow for great search engine optimisation while also being lighting fast while also allowing for real time updates from a database. Perfect for landing or brochure based websites.",
     image: LandingImage,
-    tags: ["JAM stack", "Gatsby", "AWS-S3", "Netlify"],
+    tags: ["JAM stack", "Gatsby", "AWS", "Netlify"],
   }
 
   return (
